9a6c9b2a8543d9135dad822922a6af62459bfa14
Imrane QERCHI <imrane.qerchi@edu.univ-fcomte.fr>
2024-11-28 12:33:22 +0100

    Ajout d'un deuxième stylme au bouton

--- src/components/btnDefault.vue
+++ src/components/btnDefault.vue
@@ -1,23 +1,30 @@
 <script setup lang="ts">
-// Propriétés dynamiques pour le texte du bouton et la flèche
 defineProps({
   text: {
     type: String,
-    default: 'MES PROJETS' // Texte par défaut
+    default: "Bouton",
   },
   showArrow: {
     type: Boolean,
-    default: true // Par défaut, la flèche est visible
-  }
-})
+    default: true,
+  },
+  variant: {
+    type: String as () => "solid" | "outline",
+    default: "solid",
+  },
+});
 </script>
 
 <template>
   <button
-    class="flex items-center justify-center rounded-full bg-orange-500 px-8 py-3 font-semibold text-white shadow-md transition-all duration-300 hover:bg-orange-600"
+    :class="[
+      'font-semibold rounded-full px-8 py-3 flex items-center justify-center transition-all duration-300 shadow-md',
+      variant === 'solid'
+        ? 'bg-orange-500 text-white hover:bg-orange-600'
+        : 'border border-orange-400 text-orange-400 bg-transparent hover:bg-orange-400 hover:text-white',
+    ]"
   >
     {{ text }}
-    <!-- Affiche la flèche uniquement si showArrow est true -->
     <span v-if="showArrow" class="ml-2 text-xl">→</span>
   </button>
 </template>
