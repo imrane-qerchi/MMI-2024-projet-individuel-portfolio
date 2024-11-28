c3113396aaf1ab2eb5a43083e2d23e2730adbc7b
Imrane QERCHI <imrane.qerchi@edu.univ-fcomte.fr>
2024-11-28 10:56:29 +0100

    Test du bouton dans le guide de style

--- src/pages/guide-style.vue
+++ src/pages/guide-style.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import CardProjet from '@/components/cardProjet.vue'
 import { pb } from '@/backend';
+import BtnDefault from '@/components/btnDefault.vue';
 
 const mesProjets = await pb.collection('projets').getFullList()
 </script>
@@ -9,4 +10,7 @@ const mesProjets = await pb.collection('projets').getFullList()
   <h1 class="text-center font-syne text-2xl">Guide de style</h1>
   <CardProjet v-bind="mesProjets[0]"/>
   <!-- <CardProjet v-for="unProjet in mesProjets" v-bind="unProjet" :key="unProjet.id"/> -->
+  <div>
+    <BtnDefault text="MES PROJETS" :showArrow="true" />
+  </div>
 </template>
