5af28c5c98d6f4a3eed6e4d027390df1d91ea145
Imrane QERCHI <imrane.qerchi@edu.univ-fcomte.fr>
2024-11-29 11:41:46 +0100

    Page index.vue de projets

--- src/pages/projets/index.vue
+++ src/pages/projets/index.vue
@@ -1,14 +1,33 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <script setup lang="ts">
 import { pb } from '@/backend'
+import CardProjet from '@/components/cardProjet.vue'
+import BtnDefault from '@/components/btnDefault.vue'
 
-const listeProjets = await pb.collection('projets').getFullList()
+const listeProjets = await pb.collection('projets').getFullList({ sort: '-created' })
 </script>
 
 <template>
-  <h1 class="font-syne text-2xl text-center">Mes projets</h1>
+  <div class="projets-container">
+    <!-- Titre principal -->
+    <h1 class="mt-12 px-6 text-left font-syne text-3xl">Mes projets</h1>
 
-  <ul>
-    <li v-for="projet in listeProjets" :key="projet.id">{{ projet.nom }}</li>
-  </ul>
+    <!-- Sous-titre -->
+    <p class="mt-4 px-6 text-left text-xl text-gray-500">Un avant-goût de ce que j’ai pu faire</p>
+
+    <!-- Barre horizontale -->
+    <div class="mx-6 my-6">
+      <hr class="border-t-2 border-gray-300" />
+    </div>
+
+    <!-- Liste des projets -->
+    <div class="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+      <CardProjet v-for="projet in listeProjets" :key="projet.id" v-bind="projet" />
+    </div>
+
+    <!-- Bouton explorer plus -->
+    <div class="mt-8 flex justify-center">
+      <BtnDefault text="EXPLOREZ PLUS" showArrow />
+    </div>
+  </div>
 </template>
